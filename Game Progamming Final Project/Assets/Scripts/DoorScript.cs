ef53804e4eb68f4a90f9361a50c7c341d7b8670b
TannerBadey <tbonejenkins7242@gmail.com>
2022-03-29 01:30:42 -0400

    stuff n things

---
+++ Game Progamming Final Project/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (LevelManager.enemiesLeft == 0 && other.CompareTag("Player"))
+        {
+            FindObjectOfType<LevelManager>().LevelBeaten();
+        }
+    }
+}

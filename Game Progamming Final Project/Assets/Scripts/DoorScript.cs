ac25eede6726ae69d3add4fa173991830350ea30
Mateus Aurelio <83532421+Mateus-Aurelio@users.noreply.github.com>
2022-05-01 10:14:49 -0400

    Lvl2 fix

--- Game Progamming Final Project/Assets/Scripts/DoorScript.cs
+++ Game Progamming Final Project/Assets/Scripts/DoorScript.cs
@@ -6,7 +6,7 @@ public class DoorScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (LevelManager.enemiesLeft == 0 && other.CompareTag("Player"))
+        if (LevelManager.enemiesLeft <= 0 && other.CompareTag("Player"))
         {
             FindObjectOfType<LevelManager>().LevelBeaten();
         }
